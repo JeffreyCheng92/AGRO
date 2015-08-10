2f474b38557376b40a0ec2beb228d57b0c30f94b
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-08-10 16:23:39 -0700

    wrote application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    # if user saves, log them in, else render errors
+    if @user.save
+      log_in(@user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
 
   def edit
