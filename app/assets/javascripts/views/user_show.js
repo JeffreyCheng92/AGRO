4cf5d27bf9c58fc465f9f4a4fd535471b1da20a8
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-08-11 10:27:34 -0700

    added user show view and template

---
+++ app/assets/javascripts/views/user_show.js
@@ -0,0 +1,14 @@
+AGRO.Views.userShow = Backbone.View.extend({
+  template: JST["users/user_show"],
+
+  initialize: function(options) {
+    this.listenTo(this.model, "sync", this.render);
+  },
+
+  render: function() {
+    var content = this.template({user: this.model});
+    this.$el.html(content);
+    return this;
+  },
+
+});
