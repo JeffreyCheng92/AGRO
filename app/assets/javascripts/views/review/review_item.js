66a21b4b2a328a3fcf24371c4ed3a1c879592da8
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-08-13 14:10:59 -0700

    added review item view and template

---
+++ app/assets/javascripts/views/review/review_item.js
@@ -0,0 +1,22 @@
+AGRO.Views.reviewItem = Backbone.View.extend({
+  template: JST['reviews/review_item'],
+
+  initialize: function(options) {
+    this.listenTo(this.model, 'sync', this.render);
+  },
+
+  render: function() {
+    var users = new AGRO.Collections.Users();
+    var user = users.getOrFetch(this.model.get('author_id'));
+    
+    var content = this.template({
+      username: user.get('username'),
+      review: this.model
+    });
+
+    this.$el.html(content);
+    return this;
+  }
+
+
+});
