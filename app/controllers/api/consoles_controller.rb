a3935b4c8cbad3920a48cc22888022a0159cc0bb
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-08-18 22:47:45 -0700

    fixed ordering of console games

--- app/controllers/api/consoles_controller.rb
+++ app/controllers/api/consoles_controller.rb
@@ -5,21 +5,12 @@ class Api::ConsolesController < ApplicationController
 
     if params[:letter]
       let = params[:letter].downcase
-      # game_ids = GameConsole.where('console_id = ?', params[:id])
-      #                        .pluck('game_id')
-      # @games = []
-      # game_ids.each do |game_id|
-      #   game = Game.find(game_id)
-      #   @games << game
-      # end
-      #
-      # @games = @games.select { |game| !!(game.title.downcase =~ /^#{let}/) }
 
       @games =
         Game.joins(:game_consoles)
             .where("game_consoles.console_id = ? AND
                     LOWER(games.title) LIKE ?", params[:id], "#{let}%")
-
+            .order(:title)
 
       @console.searched_games = @games
       @console.searched = true
--- app/views/api/consoles/show.json.jbuilder
+++ app/views/api/consoles/show.json.jbuilder
@@ -1,12 +1,13 @@
 json.extract!(@console, :id, :name, :searched)
 
 json.games do
-  json.array!(@console.games) do |game|
+  json.array!(@console.games.order(:title)) do |game|
     json.partial!('api/games/game', game: game, show_reviews: false, show_info: false)
   end
 end
 
 json.searched_games do
+  # if searched games unordered, do order(:title) here instead of controller
   json.array!(@console.searched_games) do |game|
     json.partial!('api/games/game', game: game, show_reviews: false, show_info: false)
   end
