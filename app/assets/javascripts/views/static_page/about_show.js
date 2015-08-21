68de7977e24537107fa072d702a5a06d7b2b1d93
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-08-21 09:51:01 -0700

    added rails mailer and ajax to contact form

--- app/assets/javascripts/views/static_page/about_show.js
+++ app/assets/javascripts/views/static_page/about_show.js
@@ -1,9 +1,26 @@
 AGRO.Views.aboutShow = Backbone.View.extend({
   template: JST["static_pages/about_show"],
 
+  events: {
+    "click .contact": "sendMail"
+  },
+
   render: function() {
     this.$el.html(this.template());
     return this;
+  },
+
+  sendMail: function(event) {
+    event.preventDefault();
+    debugger
+    var formData = this.$(".contact-form").serializeJSON();
+
+    $.ajax({
+      url: '/api/mails',
+      dataType: 'json',
+      method: 'POST',
+      data: {data: formData.contact},
+    });
   }
 
 });
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,8 @@
 class ContactMailer < ApplicationMailer
+  default from: "AGRO <contact@agr.online>"
+
+  def contact_email
+    mail(to: "JeffreyCheng92@gmail.com", subject: 'AGRO Issue')
+  end
+
 end
