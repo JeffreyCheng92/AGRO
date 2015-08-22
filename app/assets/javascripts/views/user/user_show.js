54210ed91f51d7da44422c3846028aa55e437b41
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-08-21 19:52:56 -0700

    render recently reviewed games on user show

--- app/assets/javascripts/views/user/user_show.js
+++ app/assets/javascripts/views/user/user_show.js
@@ -8,6 +8,21 @@ AGRO.Views.userShow = Backbone.View.extend({
   render: function() {
     var content = this.template({user: this.model});
     this.$el.html(content);
+    this.imageOnRender();
+    this.reviewsOnRender();
+    return this;
+  },
+
+  reviewsOnRender: function() {
+    this.model.recentReviews().forEach( function(review) {
+      var $li = $("<li>").addClass("recent-review-list-item");
+      var $link = $("<a>").attr("href", "/#games/" + review.get('id'))
+                          .html(review.escape("title"));
+      this.$(".recent-review-list").append($li.html($link));
+    }.bind(this));
+  },
+
+  imageOnRender: function() {
     if (this.model.keys().length > 1) {
       var avatar = this.model.avatar();
       //hard coding link to meet size criteria in profile page
@@ -17,7 +32,6 @@ AGRO.Views.userShow = Backbone.View.extend({
       this.$(".image-link").attr("href", avatar.get('url'))
                            .html($img);
     }
-    return this;
-  },
+  }
 
 });
